e9badc26da48587b5944659097fce2d830fd1355
Joe <joe@reddoor.com.tw>
2021-02-19 21:23:56 -0500

    add static & templates

--- app/views/frontend/index.py
+++ app/views/frontend/index.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from . import frontend
 
 
 @frontend.route('/', methods=['GET', 'POST'])
 def index():
-    return 'I am frontend'
+    return render_template('frontend/index.html', title="I am frontend")
